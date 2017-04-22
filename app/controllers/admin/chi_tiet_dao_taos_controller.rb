321b865a69865c22b55b087097c21b0b374db357
NgoVanTien <ngovantien93@gmail.com>
2017-04-22 15:03:29 +0700

    Form_Chi_Tiet_Dao_Tao_Save_Data

--- app/controllers/admin/chi_tiet_dao_taos_controller.rb
+++ app/controllers/admin/chi_tiet_dao_taos_controller.rb
@@ -6,6 +6,8 @@ class Admin::ChiTietDaoTaosController < Admin::ApplicationController
 
   def new
     @monhocs = MonHoc.mon_thuoc_khoa params[:chuong_trinh_dao_tao_id]
+    @chitietdaotaos = ChiTietDaoTao.where(chuong_trinh_dao_tao_id: params[:chuong_trinh_dao_tao_id])
+    @monhoc_daluu = @monhocs - MonHoc.where(id: @chitietdaotaos.pluck(:mon_hoc_id))
   end
 
   def show
@@ -15,7 +17,23 @@ class Admin::ChiTietDaoTaosController < Admin::ApplicationController
   end
 
   def create
-    binding.pry
+    ctdt = ChiTietDaoTao.where(chuong_trinh_dao_tao_id: params[:chuong_trinh_dao_tao_id])
+    if ctdt.any?
+      ctdt.destroy_all
+    end
+    list_hoc_ky = params[:list_hoc_ky]
+    if list_hoc_ky.present?
+      (1..params[:num_hocky].to_i).each do |num|
+        hoc_ky = "hk_" + num.to_s
+        if list_hoc_ky[hoc_ky].present?
+          mamonhoc = list_hoc_ky[hoc_ky]
+          mamonhoc.each do |val|
+            ChiTietDaoTao.create(kyhoc: num, mon_hoc_id: val, chuong_trinh_dao_tao_id: params[:chuong_trinh_dao_tao_id])
+          end
+        end
+      end
+    end
+    redirect_to new_admin_chuong_trinh_dao_tao_chi_tiet_dao_tao_path(params[:chuong_trinh_dao_tao_id])
   end
 
   private
